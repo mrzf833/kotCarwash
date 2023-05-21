ebc59577dc7eb1360b40d640318b9fb53b390f26
mrzf833 <palaktais@gmail.com>
2023-05-21 15:01:51 +0700

    add preferences datastore

--- app/src/main/java/org/d3if3090/carwash/ui/history/HistoryFragment.kt
+++ app/src/main/java/org/d3if3090/carwash/ui/history/HistoryFragment.kt
@@ -5,16 +5,27 @@ import android.util.Log
 import android.view.*
 import android.widget.Button
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.coroutines.launch
 import org.d3if3090.carwash.R
 import org.d3if3090.carwash.databinding.FragmentHistoryBinding
 import org.d3if3090.carwash.db.CarwashDb
+import org.d3if3090.carwash.db.SettingDataStore
+import org.d3if3090.carwash.db.dataStore
 
 class HistoryFragment: Fragment() {
+    private val layoutDataStore: SettingDataStore by lazy {
+        SettingDataStore(requireContext().dataStore)
+    }
     private val viewModel: HistoryViewModel by lazy {
         val db = CarwashDb.getInstance(requireContext())
         val factory = HistoryViewModelFactory(db.historyDao)
@@ -23,6 +34,7 @@ class HistoryFragment: Fragment() {
 
     private lateinit var binding: FragmentHistoryBinding
     private  lateinit var myAdapter: HistoryAdapter
+    private var isLinearLayout = true
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?, savedInstanceState: Bundle?
@@ -44,19 +56,47 @@ class HistoryFragment: Fragment() {
                 View.VISIBLE else View.GONE
             myAdapter.submitList(it)
         }
+
+        layoutDataStore.preferenceFlow.asLiveData().observe(viewLifecycleOwner) {
+            isLinearLayout = it
+            setLayout()
+            activity?.invalidateOptionsMenu()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.history_menu, menu)
+
+        val menuItem = menu.findItem(R.id.action_switch_layout)
+        setIcon(menuItem)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.menu_hapus) {
             hapusSemuaData()
             return true
+        }else if (item.itemId == R.id.action_switch_layout) {
+            lifecycleScope.launch {
+                layoutDataStore.saveLayout(!isLinearLayout, requireContext())
+            }
+            return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun setLayout() {
+        binding.recyclerView.layoutManager = if (isLinearLayout)
+            LinearLayoutManager(context)
+        else
+            GridLayoutManager(context, 2)
+    }
+    private fun setIcon(menuItem: MenuItem) {
+        val iconId = if (isLinearLayout)
+            R.drawable.ic_grid_view_24
+        else
+            R.drawable.ic_view_list_24
+        menuItem.icon = ContextCompat.getDrawable(requireContext(), iconId)
     }
 
     private fun hapusSemuaData(){
